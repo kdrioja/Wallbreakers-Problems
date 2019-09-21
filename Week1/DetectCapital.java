d1155a672fde1456aa777cc8caeca95553479658
Kenia Rioja <kdrioja@gmail.com>
2019-09-20 20:01:23 -0700

    Fixed Detect Capital problem.

--- Week1/DetectCapital.java
+++ Week1/DetectCapital.java
@@ -1,15 +1,3 @@
-/**
-case 1:
-    first letter is capital
-    second is also capital and so therefore all the other ones should be
-
-case 2:
-    only first letter is capital
-    
-case 3:
-    all letters are lowercase
-**/
-
 class Solution {
     public boolean detectCapitalUse(String word) {
         if (word == null || word.length() == 0) {
@@ -30,25 +18,48 @@ class Solution {
             char c = word.charAt(i);
             
             if (i == 1) {
-                if (firstCapital && Character.isUpperCase(c)) {
-                    allCapitals = true;
-                }
-                else if (allLowerCase && Character.isUpperCase(c)) {
+                if (firstCapital) {
+                    if (Character.isUpperCase(c)) {
+                        allCapitals = true;
+
+                    }
+                } 
+                else if (allLowerCase) {
+                    if (Character.isUpperCase(c)) {
                         return false;
+                    }
                 }
             }
             
-            if (allCapitals && Character.isLowerCase(c)) {
+            if (allCapitals) {
+                if (Character.isLowerCase(c)) {
                     return false;
+                }
             }
-            else if (allLowerCase && Character.isUpperCase(c)) {
+            else if (allLowerCase) {
+                if (Character.isUpperCase(c)) {
                     return false;
+                }
             }
-            else if (firstCapital && Character.isUpperCase(c)) {
+            else if (firstCapital) {
+                if (Character.isUpperCase(c)) {
                     return false;
+                }
             }
         }
         
         return true;
     }
 }
+
+/**
+case 1:
+    first letter is capital
+    second is also capital and so therefore all the other ones should be
+
+case 2:
+    only first letter is capital
+    
+case 3:
+    all letters are lowercase
+**/
